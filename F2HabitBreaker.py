120e6e390f1bd5345a632138a4a40b10fac9fc96
BuRny <gamingburny@gmail.com>
2019-07-20 15:59:24 +0200

    Add requirements.txt Change from csv to json format Add usage of SC2 client API (check if in game or menu) Automatically change opacity to 0 when in menu, else default value when in game Add .gitignore Update README.md Apply Black formatter

--- F2HabitBreaker.py
+++ F2HabitBreaker.py
@@ -1,106 +1,134 @@
-import sys, ctypes, win32api, functools, csv
+import sys, functools, json
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QMainWindow, QMessageBox, QMenu, QAction
+from screeninfo import get_monitors
+import requests
+
 
 class F2HabitBreaker(QMainWindow):
     def __init__(self):
-            super().__init__() 
+        super().__init__()
 
-            self.availableMonitors=[]
-            monitors = win32api.EnumDisplayMonitors()
-            for monitor in monitors:    
-                # monitor[2] => (0, 0, 1920, 1080)    [ x start, y start, x end, y end ]    
-                
-                # get the resolution so we can find the correct position
-                X = monitor[2][2] - monitor[2][0]
-                Y = monitor[2][3] - monitor[2][1]
-                resolution=str(X)+"x"+str(Y)
-                
-                # offsets to add to button position to put it on the right monitor
-                xOffset=monitor[2][0]
-                yOffset=monitor[2][1]
-                
-                self.availableMonitors.append({'resolution': resolution, 'xOffset': xOffset, 'yOffset': yOffset})
-                
+        # One of "game" or "menu"
+        self.location = "game"
+        self.ui_url = "http://localhost:6119/ui"
+        # Target opacity level of the window
+        self.window_target_opacity = 0
 
-            self.placeWindow(0)
-            
-            self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
-            self.show()
-            
-            
+        self.availableMonitors = []
+        monitors = get_monitors()
+        for monitor in monitors:
+            # Monitor:
+            # => [monitor.x, monitor.y, monitor.width, monitor.height]
+            # => [0, 0, 1920, 1080]    [ x start, y start, x end, y end ]
+
+            # Get the resolution so we can find the correct position
+            X = monitor.width
+            Y = monitor.height
+            resolution = str(X) + "x" + str(Y)
+
+            # Offsets to add to button position to put it on the right monitor
+            xOffset = monitor.x
+            yOffset = monitor.y
+
+            self.availableMonitors.append({"resolution": resolution, "xOffset": xOffset, "yOffset": yOffset})
+
+
+        # Run self.tick function every 1000ms
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.tick)
+        self.timer.start(5000)
+
+        self.placeWindow(0)
+
+        # Dont render frame, and always have window on top
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
+        self.show()
+
+    def updateGameLocation(self):
+        """ Use the SC2 client API to get information about the location in game, if the usesr is in menu or in game. """
+        client_api_ui_response = requests.get(self.ui_url)
+        if client_api_ui_response.status_code == 200:
+            client_api_ui_data = client_api_ui_response.json()
+            if client_api_ui_data["activeScreens"]:
+                self.location = "menu"
+            else:
+                self.location = "game"
+
+    def updateTransparency(self):
+        """ Use the information gathered above to set the opacity to 0 if user is out of game. """
+        if self.location == "game":
+            self.setProperty("windowOpacity", self.window_target_opacity)
+        elif self.location == "menu":
+            self.setProperty("windowOpacity", 0)
+
+    def tick(self):
+        self.updateGameLocation()
+        self.updateTransparency()
+
     def contextMenuEvent(self, event):
         menu = QMenu(self)
         for monitor in self.availableMonitors:
-            menu.addAction(QAction(str(self.availableMonitors.index(monitor))+": "+monitor['resolution'], self, triggered=functools.partial(self.placeWindow, self.availableMonitors.index(monitor))))
-                
-        menu.addSection('')
+            menu.addAction(
+                QAction(
+                    str(self.availableMonitors.index(monitor)) + ": " + monitor["resolution"],
+                    self,
+                    triggered=functools.partial(self.placeWindow, self.availableMonitors.index(monitor)),
+                )
+            )
+
+        menu.addSection("")
         menu.addAction(QAction("Quit", self, triggered=self.quit))
         menu.exec_(event.globalPos())
-    
-    
-    def getPositions(self): 
-        positions=[]
-        with open('F2HabitBreaker.csv', 'rt') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-            for row in reader:
-                try: # try to get position with rgba
-                    positions.append({ 
-                        'resolution': row[0], 
-                        'x': int(row[1]), 
-                        'y': int(row[2]), 
-                        'xOffset': int(row[3]), 
-                        'yOffset': int(row[4]),
-                        'r': int(row[5]),
-                        'g': int(row[6]),
-                        'b': int(row[7]),
-                        'a': float(row[8])                    
-                    })
-                except:
-                    try: # if we cant, try to get old style position only
-                        positions.append({ 
-                            'resolution': row[0], 
-                            'x': int(row[1]), 
-                            'y': int(row[2]), 
-                            'xOffset': int(row[3]), 
-                            'yOffset': int(row[4]),     
-                            'r': 0,
-                            'g': 0,
-                            'b': 0,
-                            'a': 1                               
-                        })
-                    except:
-                        pass
-        return positions
-        
+
+    def getProfiles(self):
+        with open("F2HabitBreaker.json", "r") as f:
+            profiles = json.load(f)
+            return profiles
+
     def placeWindow(self, monitorId):
         # find the right position for this resolution
-        position=None
-        for f2Position in self.getPositions():
-            if f2Position['resolution']==self.availableMonitors[monitorId]['resolution']:
-                position=f2Position
-        
+        profile = None
+        for f2Profile in self.getProfiles():
+            if f2Profile["resolution"] == self.availableMonitors[monitorId]["resolution"]:
+                profile = f2Profile
+
         # if we havent found it then we need to quit                  (uh quitting probably isnt actually the best option?)
-        if position==None:
-            print('Unsupported resolution: ')
-            
-            QMessageBox.information(self, 'Message',
-                "Unsupported Resolution: "+self.availableMonitors[monitorId]['resolution'], QMessageBox.Ok)
+        if profile is None:
+            print("Unsupported resolution: ")
+
+            QMessageBox.information(
+                self,
+                "Message",
+                "Unsupported Resolution: " + self.availableMonitors[monitorId]["resolution"],
+                QMessageBox.Ok,
+            )
             self.quit()
         # or if we have found it, we can move/resize the window into place
         else:
-            self.resize(position['x'],
-                        position['y'])
-            self.move(position['xOffset'] + self.availableMonitors[monitorId]['xOffset'],
-                        position['yOffset'] + self.availableMonitors[monitorId]['yOffset'])
-            self.setStyleSheet("QMainWindow { background-color: rgba("+str(position['r'])+", "+str(position['g'])+", "+str(position['b'])+", 1); }")
-            self.setProperty("windowOpacity", position['a'] if position['a']>0 else 0.01 )
+            self.resize(profile["width"], profile["height"])
+            self.move(
+                profile["x-offset"] + self.availableMonitors[monitorId]["xOffset"],
+                profile["y-offset"] + self.availableMonitors[monitorId]["yOffset"],
+            )
+            self.setStyleSheet(
+                "QMainWindow { background-color: rgba("
+                + str(profile["red"])
+                + ", "
+                + str(profile["green"])
+                + ", "
+                + str(profile["blue"])
+                + ", 1); }"
+            )
+            self.window_target_opacity = profile["transparency"] if profile["transparency"] > 0 else 0.01
+            self.setProperty("windowOpacity", self.window_target_opacity)
 
     def quit(self):
-        sys.exit()     
+        sys.exit()
 
-        
-if __name__ == '__main__':   
+
+if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    F2=F2HabitBreaker()
+    F2 = F2HabitBreaker()
     app.exec_()
--- README.md
+++ README.md
@@ -10,3 +10,5 @@ If your resolution isn't supported you can add a line to `F2HabitBreaker.csv` in
 You can optionally add additional fields to control the colour and opacity of the overlay in the format *Resolution,Width,Height,xPosition,yPosition,r,g,b,opacity*
 
 Right click on the overlay to switch monitors or quit. 
+
+Currently it prevents mouse clicking on the command card. To change it back to blocking the F2 button, remove the 2nd entry in the `F2HabitBreaker.json` file surrounded by the curly brackets `{}`
